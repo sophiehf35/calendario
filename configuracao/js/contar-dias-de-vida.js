b347227a61ca7edb3538abb67d0dc8d16e1a02bf
sophiehf35 <sophiehf35@gmail.com>
2024-03-25 13:12:38 -0300

    Upload inicial

--- configuracao/js/contar-dias-de-vida.js
+++ configuracao/js/contar-dias-de-vida.js
@@ -131,18 +131,14 @@ document.addEventListener('DOMContentLoaded', function() {
         ocultaNotificacao(verificaTipoAlerta(divNotificacaoContador), inputDataNascimento, divNotificacaoContador);
     });
 
-    inputDataFinal.addEventListener("focus", function() {
-        ocultaNotificacao(verificaTipoAlerta(divNotificacaoContador), inputDataFinal, divNotificacaoContador);
-    });
-
-function calculaMeses(dataNascimento,dataFinal, roundUpFractionalMonths) {
+function calculaMeses(dataNascimento, dataAtual, roundUpFractionalMonths) {
 
     var startDate = dataNascimento;
-    var endDate = dataFinal;
+    var endDate = dataAtual;
     endDate.setDate(endDate.getDate() + 2)
     var inverse=false;
     
-    if(dataNascimento>dataFinal) {
+    if(dataNascimento>dataAtual) {
         startDate=date2;
         endDate=date1;
         inverse=true;
@@ -165,68 +161,19 @@ function calculaMeses(dataNascimento,dataFinal, roundUpFractionalMonths) {
     return (inverse?-1:1)*(yearsDifference*12+monthsDifference+monthCorrection);
 };
 
-function calculaIntervalo(dataNascimentoStr, dataFinalStr, incluirDiaInicial, incluirFeriadoseFacultativos, datasEspeciais) {
+function calculaIntervalo(dataNascimentoStr, dataAtualStr) {
 
     var dataNascimento = new Date(dataNascimentoStr);
-    var dataFinal = new Date(dataFinalStr);
-    
-    //INCLUI O DIA INICIAL NA CONTAGEM CASO FOR TRUE
-    if(incluirDiaInicial === 'true') {
-        dataNascimento.setDate(dataNascimento.getDate() - 1);
-    }
-
-    // Função para verificar se uma data é feriado
-    function isFeriado(data) {
-        return datasEspeciais.some(feriado => {
-            var feriadoDataObj = new Date(feriado.data.split('/').reverse().join('-'));
-            return data.toDateString() === feriadoDataObj.toDateString() && feriado.tipo === 'Feriado';
-        });
-    }
-
-    // Função para verificar se uma data é ponto facultativo
-    function isFacultativo(data) {
-        return datasEspeciais.some(facultativo => {
-            var facultativoDataObj = new Date(facultativo.data.split('/').reverse().join('-'));
-            return data.toDateString() === facultativoDataObj.toDateString() && facultativo.tipo === 'Facultativo';
-        });
-    }
+    var dataAtual = new Date(dataAtualStr);
 
     // Inicializa variáveis
     var diasUteis = 0;
-    var feriados = 0;
-    var facultativos = 0;
-    var dataAtual = new Date(dataNascimento);
     var conteudoTabela = '';
 
     // Loop através dos dias entre as datas
-    while (dataAtual <= dataFinal) {
-        if (isFeriado(dataAtual)) {
-            feriados++;
-            var feriado = datasEspeciais.find(feriado => {
-                var feriadoDataObj = new Date(feriado.data.split('/').reverse().join('-'));
-                return dataAtual.toDateString() === feriadoDataObj.toDateString() && feriado.tipo === 'Feriado';
-            });
-            
-            if (feriado) {
-                conteudoTabela += `<tr><td class="dtr-control">${feriado.data.substr(0, 10).split('-').reverse().join('/')}</td><td class="dtr-control">${feriado.nome}</td><td class="dtr-control">Feriado</td></tr>`;
-            }
+    while (dataNascimento <= dataAtual) {
 
-        }
-
-        if (isFacultativo(dataAtual)) {
-            facultativos++;
-            var facultativo = datasEspeciais.find(facultativo => {
-                var facultativoDataObj = new Date(facultativo.data.split('/').reverse().join('-'));
-                return dataAtual.toDateString() === facultativoDataObj.toDateString() && facultativo.tipo === 'Facultativo';
-            });
-
-            if (facultativo) {
-                conteudoTabela += `<tr><td class="dtr-control">${facultativo.data.substr(0, 10).split('-').reverse().join('/')}</td><td class="dtr-control">${facultativo.nome}</td><td class="dtr-control">Ponto Facultativo</td></tr>`;
-            }
-
-        }
-        
-        if ((dataAtual.toDateString() !== dataNascimento.toDateString() && !isFeriado(dataAtual) && dataAtual.getDay() !== 5 && dataAtual.getDay() !== 6)) {
+        if ((dataAtual.toDateString() !== dataNascimento.toDateString() && dataNascimento.getDay() !== 5 && dataNascimento.getDay() !== 6)) {
             diasUteis++;
         }
 
@@ -235,16 +182,14 @@ function calculaIntervalo(dataNascimentoStr, dataFinalStr, incluirDiaInicial, in
     }
 
     //CALCULA OS DIAS CORRIDOS
-    var diasCorridos = Math.floor((dataFinal - dataNascimento) / (1000 * 60 * 60 * 24));
+    var diasCorridos = Math.floor((dataAtual - dataNascimento) / (1000 * 60 * 60 * 24));
 
     var tabela = `<div style="padding-bottom: 25px;" class="container"><div><table style="margin-top:0px;" class="table tabela-customizada table-striped table-bordered dt-responsive nowrap dataTable no-footer dtr-inline"><thead style="background-color: var(--cor-primaria); color: #FFF;"><tr><th>DATA</th><th>NOME</th><th>TIPO</th></tr></thead><tbody>${conteudoTabela}</tbody></table></div></div>`;
 
     return {
         diasCorridos: diasCorridos,
         diasUteis: diasUteis,
-        feriados: feriados,
-        facultativos: facultativos,
-        meses: calculaMeses(dataNascimento, dataFinal),
+        meses: calculaMeses(dataNascimento, dataAtual),
         sectionTabela: conteudoTabela !== '' ? tabela : ''
     };
 }
@@ -252,13 +197,11 @@ function calculaIntervalo(dataNascimentoStr, dataFinalStr, incluirDiaInicial, in
 //RECUPERA OS PARÂMETROS DA URL
 var urlParams = new URLSearchParams(window.location.search);
 
-if (urlParams.has('data_inicial') && urlParams.has('data_final') && urlParams.has('incluir_dia_inicial')) {
+if (urlParams.has('data_nascimento') && urlParams.has('data_atual')) {
 
     //RECUPERA E SETA OS PARÂMETROS
-    var dataNascimento = urlParams.get('data_inicial');
+    var dataNascimento = urlParams.get('data_nascimento');
     var dataFinal = urlParams.get('data_final');
-    var incluirDiaInicial = urlParams.get('incluir_dia_inicial');
-    var incluirFeriadoseFacultativos = urlParams.get('incluir_feriados_e_facultativos');
     var feriados_e_facultativosArray = [];
     
     //RECUPERA OS FERIADOS E FACULTATIVOS DA URL CASO ESTEJA SOLICITADO PARA INCLUIR
@@ -269,16 +212,14 @@ if (urlParams.has('data_inicial') && urlParams.has('data_final') && urlParams.ha
     
     //TRATA DATA PARA FORMATO UNIVERSAL
     var dataNascimentoUniversal = dataNascimento.substring(0, 10).split('/').reverse().join('-');
-    var dataFinalUniversal = dataFinal.substring(0, 10).split('/').reverse().join('-');
+    var dataAtualUniversal = dataFinal.substring(0, 10).split('/').reverse().join('-');
 
-    var intervalo = calculaIntervalo(dataNascimentoUniversal, dataFinalUniversal, incluirDiaInicial, incluirFeriadoseFacultativos, feriados_e_facultativosArray);
+    var intervalo = calculaIntervalo(dataNascimentoUniversal, dataAtualUniversal);
 
     var diasUteis = intervalo.diasUteis;
     var diasCorridos = intervalo.diasCorridos;
     var meses = intervalo.meses;
     var semanas = Math.floor(intervalo.diasCorridos / 7);
-    var feriados = intervalo.feriados;
-    var facultativos = intervalo.facultativos;
 
     var todasAsSecoes = document.querySelectorAll('section');
     todasAsSecoes.forEach(function(secao) {
@@ -294,19 +235,13 @@ if (urlParams.has('data_inicial') && urlParams.has('data_final') && urlParams.ha
     divAcaoContador.innerHTML = `<div class="box_account">
         <div style="box-shadow: 0px 0px 30px 0px rgb(0 0 0 / 10%); background-color: #fff; padding: 25px;">
             <div class="row">
-                <div class="col-md-4">
+                <div class="col-md-6">
                     <div class="form-group">
                     <label class="label"><b style="font-weight:600">DATA INICIAL</b></label>
                     <input style="font-size: 1.5rem;" type="text" value="${dataNascimento}" class="form-control add__date-one" id="data_inicial" readonly="readonly">
                     </div>
                 </div>
-                <div class="col-md-4">
-                    <div class="form-group">
-                    <label class="label"><b style="font-weight:600">DATA FINAL</b></label>
-                    <input style="font-size: 1.5rem;" type="text" value="${dataFinal}" class="form-control add__date-two" id="data_final" readonly="readonly"/>
-                    </div>
-                </div>
-                <div class="col-md-4 mt-3 mt-md-4">
+                <div class="col-md-6 mt-3 mt-md-4">
                     <a href="/ferramentas/contador-de-dias-calculadora-prazo-entre-datas" class="align-middle"><button style="width:100%;" class="btn_1 full-width fe-pulse" type="button">FAZER NOVO CÁLCULO</button></a>
                 </div>
             </div>
@@ -356,28 +291,6 @@ if (urlParams.has('data_inicial') && urlParams.has('data_final') && urlParams.ha
                     </div>
                     </div>
                 </div>
-                ${incluirFeriadoseFacultativos === 'true' ? `
-                    <div class="col-xl-6 col-sm-6 col-6">
-                        <div class="card">
-                            <div class="card-content">
-                                <div class="card-body">
-                                    <h2 class="titulo_item_contador">${feriados !== 0 ? String(feriados).padStart(2, '0') : 'Nenhum'}</h2>
-                                    <span class="descricao_item_contador">${feriados >= 2 ? 'Feriados' : 'Feriado'}</span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-xl-6 col-sm-6 col-6">
-                        <div class="card">
-                            <div class="card-content">
-                                <div class="card-body">
-                                    <h2 class="titulo_item_contador">${facultativos !== 0 ? String(facultativos).padStart(2, '0') : 'Nenhum'}</h2>
-                                    <span class="descricao_item_contador">${facultativos >= 2 ? 'Facultativos' : 'Facultativo'}</span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                ` : ''}
             </div>
         </div>
     </section>`;
